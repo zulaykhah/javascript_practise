3d206b014efd8f6b2971c38bed80552beb6e9c6c
Zulaykhah <zulaykhahogunmola@gmail.com>
2019-11-23 19:21:34 +0100

    my apology message

---
+++ Apology.js
@@ -0,0 +1,8 @@
+const apology = ("I will never spam my friends again.  ");
+console.log(apology.repeat(100));
+//function apology(){
+  //  for (let i = 1; i <= 100; i++){
+    //return('I will never spam my friends again');
+    //}
+//}
+//console.log(apology());
